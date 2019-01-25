69d135f63fdb95c3fdec5460716130ac4edcab10
bhavsingh2017 <bhavkirs@usc.edu>
2019-01-24 23:26:02 -0800

    kind of home page

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Easing, AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+import FadeIn from 'react-native-fade-in-image';
 
 export default class App extends React.Component {
   render() {
     return (
         //Dude this is freaking sick
       <View style={styles.container}>
-        <Text style={styles.titleText}>This is our roomate app, developed by Bhav & Yami</Text>
+
+        <Image style={ImageStyles.stretch} source={require('/Users/Bhav/allPrograms/RoomateApp/assets/logo.png')}/>
+        <Text style={styles.titleText}>Den</Text>
       </View>
     );
   }
 }
+/*
+class FadeInView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeAnim: new Animated.Value(0), // init opacity 0
+    };
+  }
+  componentDidMount() {
+    Animated.timing(
+      // Uses easing functions
+      this.state.fadeAnim, // The value to drive
+      {toValue: 1}, // Configuration
+    ).start(); // Don't forget start!
+  }
+  render() {
+    return (
+      <Animated.View // Special animatable View
+        style={{opacity: this.state.fadeAnim}}>
+        {' '}
+        // Binds
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+*/ 
+
+
+
+
+const ImageStyles = StyleSheet.create({
+  stretch:{
+    width: 250,
+    height: 250,
+    borderRadius: 70,
+  },
+});
+
 
 const styles = StyleSheet.create({
   container: {
@@ -21,10 +63,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   baseText: {
-    fontFamily: 'Cochin',
+    fontFamily: 'Times New Roman',
   },
   titleText: {
-    fontSize: 12,
+    fontSize: 50,
     fontWeight: 'bold',
   },
 });
