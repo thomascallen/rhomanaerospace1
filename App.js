5563e3e599ea0f6cdf74c743ea8df732a52fb7ff
Eliyam Levy <eliyamlevy@Eliyams-MacBook-Pro.local>
2019-02-01 18:47:57 -0800

    Added gradient background

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Keyboard, Button, Animated, Easing, TextInput, AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+import { Keyboard, Button, Animated, Easing, TextInput, AppRegistry, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { Divider } from 'react-native-elements';
 //import { StackNavigator } from 'react-navigation';
 
@@ -13,70 +13,79 @@ export default class App extends React.Component {
     return (
         //Fade in Homescreen on mount (on loading basically)
       
-    
+      //ImageBackground sets the gradient background for the app. note: background color overides this
+      <ImageBackground source={require("./assets/gradientBack.png")} style={{width: '100%', height: '100%'}}>
+        {/* .container just sets spacing for each component in the main container */}
+        <View style={styles.container}>
+          {/* logo */}
+          <FadeInView style={{width: 250, height: 250, backgroundColor: 'transparent'}}>
+            <Image style={ImageStyles.stretch} source={require('./assets/logoNoBack.png')}/> 
+          </FadeInView>
+          
+          {/* Email textBox */}
+          <View style={styles.textBox}>
+            <MakeTextInputBox placeholder={" Email"} autoComplete={"email"} keyboardType={"email-address"} />
+          </View>
 
-      <View style={styles.container}>
+          {/* newline for spacing */}
+          <View style={{fontSize: 0}}>
+            <Text></Text>
+          </View>
+
+          {/* Password textBox */}
+          <View style={styles.textBox}>
+            <MakeTextInputBox textContentType={"password"} placeholder={" Password"} secureTextEntry={true}/>
+          </View>
+
+          {/* newline for spacing */}
+          <View style={{fontSize: 0}}>
+            <Text></Text>
+          </View>
+
+          {/* Login Button */}
+          <View style={styles.button}>
+            <Button onPress={LogIn} color="rgb(35, 35, 35)" title="Log In"/>
+          </View>
+
+          {/* more spacing */}
+          <View style={{fontSize: 0}}>
+            <Text></Text>
+            <Text></Text>
+          </View>
+
+          {/* text */}
+          <View style={{fontSize: 20}}>
+          <Text style={styles.textRegister}>───── Or Create An Account  ─────</Text>
+          </View>
+
+          {/* more spacing */}
+          <View style={{fontSize: 0}}>
+            <Text></Text>
+            <Text></Text>
+          </View>
         
-        
-
-        <FadeInView style={{width: 250, height: 250, backgroundColor: 'transparent'}}>
-          <Image style={ImageStyles.stretch} source={require('./assets/logoNoBack.png')}/> 
-        </FadeInView>
-
-        
-        
-        <View style={styles.textBox}>
-          <MakeTextInputBox placeholder={" Email"} autoComplete={"email"} keyboardType={"email-address"} />
+          {/* createAccount button */}
+          <View style={styles.button}>
+            <Button onPress={SignUp} color="rgb(35, 35, 35)" title="Sign Up"/>
+          </View>
         </View>
-
-        <View style={{fontSize: 0}}>
-          <Text></Text>
-        </View>
-        
-        <View style={styles.textBox}>
-          <MakeTextInputBox placeholder={" Password"} textContentType={"password"} secureTextEntry={true}/>
-        </View>
-
-         <View style={{fontSize: 0}}>
-          <Text></Text>
-        </View>
-
-        <View style={styles.button}>
-          <Button onPress={LogIn} color="rgb(35, 35, 35)" title="Login"/>
-        </View>
-
-        <View style={{fontSize: 0}}>
-          <Text></Text>
-          <Text></Text>
-        </View>
-
-        <View style={{fontSize: 20}}>
-         <Text style={styles.textRegister}>───── Or Create An Account  ─────</Text>
-        </View>
-
-        <View style={{fontSize: 0}}>
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-        </View>
-      
-        <View style={styles.button}>
-          <Button onPress={SignUp} color="rgb(35, 35, 35)" title="Create an Account"/>
-        </View>
-      </View>
+        </ImageBackground>
     );
   }
 }
+
+//Function called by Login button
 function LogIn(){
 //if valid return 1, else return -1
   console.log("got it");
 }
 
+//Function called by Signup button
 function SignUp(){
 //navigate to signup screen
 }
 
-
+//Definition of textInput component
 class MakeTextInputBox extends Component{
   constructor(props){
     super(props);
@@ -110,11 +119,13 @@ class MakeTextInputBox extends Component{
     this.onSubmitEditing=Keyboard.dismiss;
     this.textAlignVertical= "center";
 
+    // //TODO:
+    // //Attempt to make class more modular
     // this.secureTextEntry = function(){
-    //   if(this.textContentType=="password"){
-    //     true
+    //   if(this.textContentType.localCompare("password")==0){
+    //     return "true"
     //   }else{
-    //     false
+    //     return "false"
     //   }
     // };
 
@@ -198,7 +209,7 @@ const styles = StyleSheet.create({
     flex: 2,
     //the teal color value is rgb(102,255,204)
     //
-    backgroundColor: "#00dfb0",
+    //backgroundColor: "#00dfb0",
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 180
