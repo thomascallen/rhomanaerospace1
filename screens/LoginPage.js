97f52ccbe35006bc3000c0b0b8390b88c57cf61e
bhavsingh2017 <bhavkirs@usc.edu>
2019-07-27 22:00:49 -0700

    updated

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -36,7 +36,7 @@ export default class LoginPage extends React.Component{
 
   connectMarkers(){
       //connect the markers here//
-      if(this.state.markers.length===this.state.waypointCount){
+      if(this.state.markers.length===this.state.waypointCount && this.state.waypointCount!==0){
         //then it is full and now we need to connect them
         /************************TODO************************ */
         console.log("TARGET HIT!!!");
@@ -52,14 +52,13 @@ export default class LoginPage extends React.Component{
         )
   }
 }
-
   removeMarkers(){
     //remove the markers here////
     this.setState({markers: [], waypointCount: 0});
   }
 
   addToMapMarkers(myElement){
-    console.log("I got a touch response at "+myElement);
+    console.log("I got a touch response!");
     if(this.state.markers.length<this.state.waypointCount){
       console.log(myElement);
       console.log("Adding a new Marker!");
@@ -75,55 +74,65 @@ export default class LoginPage extends React.Component{
 
     render() {
       console.log("I'm going to render the map now", this.state);
-      var data = [["4", "3", "2", "1"], ["Land at Last Waypoint", "Return to Launch Point"]];
+      var data = [[1, 2, 3,4], 
+      ["Land at Last Waypoint", "Return to Launch Point"]];
         return (
-            <MapView
-              style={{
-                flex: 1
-              }}
-              provider="google"
-              initialRegion={this.state.initalFlightPin}
-              onPress={ (event) => 
-                this.addToMapMarkers(event.nativeEvent.coordinate)}
-              >
-              {this.state.markers.map((marker, index) =>{
-                return(
-                  <MapView.Marker
-                  key={index}
-                    coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
-                    title={`Marker ${index}`}
-                    description={"Waypoint added by user"}
-                  />
-                )
-              })}
-             
-              <MapView.Marker
-              coordinate={{latitude: this.state.initalFlightPin.latitude, longitude: this.state.initalFlightPin.longitude}}
-              title={"Pin #1"}
-              description={"Initial Flight Pin"}
-              />
-              {this.connectMarkers()}
+          <View  style={{
+            flex: 1,
+      
+          }}>
             
-              <View style={{flex: 1}}>
+            
+            <MapView
+                style={{
+                  flex: 4,
+                  bottom: 0,
+                }}
+                provider="google"
+                initialRegion={this.state.initalFlightPin}
+                onPress={ (event) => 
+                  this.addToMapMarkers(event.nativeEvent.coordinate)}
+                >
                 
-                  <DropdownMenu
-                    style={{flex: 1, padding: 10}}
-                    bgColor={'white'}
-                    tintColor={'#666666'}
-                    activityTintColor={'green'}
-                    // arrowImg={}      
-                    // checkImage={}   
-                    // optionTextStyle={{color: '#333333'}}
-                    // titleStyle={{color: '#333333'}} 
-                    // maxHeight={300} 
-                    handler={(selection, row) => this.setState({markers: this.state.markers, waypointCount: data[selection][row]})}
-                    data={data}
-                  />
-              </View>
- 
 
-          </MapView>
+                {this.state.markers.map((marker, index) =>{
+                  return(
+                    <MapView.Marker
+                    key={index}
+                      coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+                      title={`Marker ${index}`}
+                      description={"Waypoint added by user"}
+                    />
+                  )
+                })}
+              
+                <MapView.Marker
+                coordinate={{latitude: this.state.initalFlightPin.latitude, longitude: this.state.initalFlightPin.longitude}}
+                title={"Pin #1"}
+                description={"Initial Flight Pin"}
+                />
+                {this.connectMarkers()}
+
+              </MapView>
+              <View style={{flex: 1.7}}>
+              <DropdownMenu
+                style={{ padding: 10,left:0,justifyContent: 'center' , alignSelf: 'flex-end'}}
+                bgColor={'white'}
+                tintColor={'#666666'}
+                activityTintColor={'green'}
+                // arrowImg={}      
+                // checkImage={}   
+                // optionTextStyle={{color: '#333333'}}
+                // titleStyle={{color: '#333333'}} 
+                handler={(selection, row) => this.setState({markers: this.state.markers, waypointCount: data[selection][row]})}
+                data={data}
+              />
+            </View>
+          </View>
           
+              
+              
+       
             );
           }
         }
